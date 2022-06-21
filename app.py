c4918c49a3ac0c80634d15befe4fe655b1ffd1f1
Jon Phillips <jon7035@gmail.com>
2022-06-20 22:51:43 -0400

    updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+stores = [
+    {
+        'name': 'Dollar General'
+        'items': [
+            {
+            'name': 'fake jordans'
+            'price': 29.00
+            }
+        ]
+    }
+]
 
-@app.route('/')
-def home():
-    return "Help Me!"
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 app.run(port=3000)
