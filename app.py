bf3a4bc6610b99d88298a8638ae603167033a643
JonathanPhillips <jon7035@gmail.com>
2022-06-22 00:19:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,60 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+
+from security import authenticate, identity
 
 app = Flask(__name__)
-stores = [
-    {
-        'name': 'Dollar General',
-        'items': [
-            {
-            'name': 'fake jordans',
-            'price': 29.99
-            }
-        ]
-    }
-]
+app.secret_key = 'jose'
+api = Api(app)
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+jwt = JWT(app,authenticate, identity)
 
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
+items = []
 
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    return jsonify({'message': 'store not found'})
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
 
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item is not None else 404        
 
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message': 'store not found'})
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {"message": "An item with name '{}' already exits.".format(name)}, 400
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
 
-@app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['items']})
-    return jsonify({'message': 'store not found'})
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
 
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
-app.run(port=3000)
+        
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=3000, debug=True)
